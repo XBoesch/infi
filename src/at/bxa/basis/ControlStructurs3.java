771777b8b1a63c718246b1628c2402f67d3de421
XBoesch <boeschxaver@gmail.com>
2023-10-18 10:52:29 +0200

    ControlStructurs4 erstelt

--- src/at/bxa/basis/ControlStructurs3.java
+++ src/at/bxa/basis/ControlStructurs3.java
@@ -9,11 +9,22 @@ public class ControlStructurs3 {
         int randomNumber = random.nextInt(100);
 
         // Gib die Zufallszahl aus
+        System.out.println("Die Zufallszahl ist: " + randomNumber);
 
-        // Wenn die Zahl kleiner ist als 20  gib aus "Mini"
-        // Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
-        // Wenn die Zahl größer als 50 ist gib aus "Large"
-
-
+        // Wenn die Zahl kleiner ist als 20, gib aus "Mini"
+        if (randomNumber < 20) {
+            System.out.println("Mini");
+        }
+        // Wenn die Zahl zwischen 20 und 50 liegt, gib aus "Medium"
+        else if (randomNumber >= 20 && randomNumber <= 50) {
+            System.out.println("Medium");
+        }
+        // Wenn die Zahl größer als 50 ist, gib aus "Large"
+        else {
+            System.out.println("Large");
+        }
     }
 }
+
+
+
